81f9ba7529db9ab32a4541abd5b3e0857aee0078
KarthikaSadaiyappan <105772916+KarthikaSadaiyappan@users.noreply.github.com>
2022-10-19 13:31:33 +0530

    Update s3.tf
    Removed duplicate provider

--- s3.tf
+++ s3.tf
@@ -1,16 +1,3 @@
-provider "aws" {
-  profile = "my-admin-profile"
-  alias  = "ap-south-1"
-  region = "ap-south-1"
-}
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 resource "aws_s3_bucket" "demo_bucket_tf" {
   bucket = "demo-tf-pipeline-bucket"
   
